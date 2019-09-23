a3f694c9f89bda0ee53cf1acbfcde1c3d7090d49
kimscott <sanaloveyou@uengine.org>
2019-09-23 15:12:10 +0900

    groupid 충돌 방지

--- src/main/java/com/example/template/config/kafka/KafkaReceiverConfig.java
+++ src/main/java/com/example/template/config/kafka/KafkaReceiverConfig.java
@@ -28,7 +28,8 @@ public class KafkaReceiverConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "mypage");
+        long seed = System.currentTimeMillis();
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "mypage"+seed);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
 
